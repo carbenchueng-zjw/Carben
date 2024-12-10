fa0710bb331cd2b964f790086d39f99d1e7e7de8
carben <382684783@qq.com>
2024-12-10 19:35:37 +0800

    rel

--- Code/cmake-build-debug/CMakeFiles/edit_cache.dir/utility.sh
+++ Code/cmake-build-debug/CMakeFiles/edit_cache.dir/utility.sh
@@ -1,4 +1,4 @@
 set -e
 
-cd /cygdrive/d/1-Git/Code/cmake-build-release
+cd /cygdrive/d/1-Git/Code/cmake-build-debug
 /cygdrive/c/Users/38268/AppData/Local/JetBrains/CLion2024.2/cygwin_cmake/bin/cmake.exe -E echo No\ interactive\ CMake\ dialog\ available.
--- Code/cmake-build-debug/CMakeFiles/rebuild_cache.dir/utility.sh
+++ Code/cmake-build-debug/CMakeFiles/rebuild_cache.dir/utility.sh
@@ -1,4 +1,4 @@
 set -e
 
-cd /cygdrive/d/1-Git/Code/cmake-build-release
+cd /cygdrive/d/1-Git/Code/cmake-build-debug
 /cygdrive/c/Users/38268/AppData/Local/JetBrains/CLion2024.2/cygwin_cmake/bin/cmake.exe --regenerate-during-build -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
