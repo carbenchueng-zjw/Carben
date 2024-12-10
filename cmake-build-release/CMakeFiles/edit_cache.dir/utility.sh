5fcf6d945cd27e470771de456e6ba667fbf2c9ad
carben <382684783@qq.com>
2024-12-10 18:22:50 +0800

    em

---
+++ cmake-build-release/CMakeFiles/edit_cache.dir/utility.sh
@@ -0,0 +1,4 @@
+set -e
+
+cd /cygdrive/d/1-Git/cmake-build-release
+/cygdrive/c/Users/38268/AppData/Local/JetBrains/CLion2024.2/cygwin_cmake/bin/cmake.exe -E echo No\ interactive\ CMake\ dialog\ available.
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "He阿萨德orld!" << std::endl;
+    return 0;
+}
