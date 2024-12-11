786f19c372d058cdccfca5adefc325a63a66c397
carbenchueng-zjw <382684783@qq.com>
2024-12-11 19:54:07 +0800

    Delete Code/cmake-build-release directory

--- Code/cmake-build-release/CMakeFiles/edit_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /cygdrive/d/1-Git/Code/cmake-build-release
-/cygdrive/c/Users/38268/AppData/Local/JetBrains/CLion2024.2/cygwin_cmake/bin/cmake.exe -E echo No\ interactive\ CMake\ dialog\ available.
--- Code/cmake-build-release/CMakeFiles/rebuild_cache.dir/utility.sh
+++
@@ -1,4 +0,0 @@
-set -e
-
-cd /cygdrive/d/1-Git/Code/cmake-build-release
-/cygdrive/c/Users/38268/AppData/Local/JetBrains/CLion2024.2/cygwin_cmake/bin/cmake.exe --regenerate-during-build -S$(CMAKE_SOURCE_DIR) -B$(CMAKE_BINARY_DIR)
